5a89d4e52c0487a07d87dbdfd35ffdf0776d478d
Dmitriy Dubson <ddubson@pivotal.io>
2017-08-14 13:47:39 -0400

    Reading filesystem as input, print to sysout

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This project is based on 
 
 ### Samples (git tags):
+
+###### Basics
 - `01-basic-job` - basic multi-step job example
 - `02-first-last-flows` - flow suffixed and prefied with a step
 - `03-...`
@@ -10,3 +12,9 @@ This project is based on
 - `05-nested-jobs` - nested parent-child jobs showcasing job within job structuring.
 - `06-pre-post-listeners` - pre-chunk and after-chunk listeners.
 - `07-job-parameters` - sending in job parameters to a job
+
+###### Readers
+
+- `08-stateless-item-reader-chunk-job` - read items in chunks from a list
+- `09-reading-from-db` - read items in chunks from database
+- `10-reading-from-fs` - read items in chunks from file system (csv format)
--- src/main/kotlin/com/ddubson/batch/JobConfiguration.kt
+++ src/main/kotlin/com/ddubson/batch/JobConfiguration.kt
@@ -1,61 +1,44 @@
 package com.ddubson.batch
 
 import com.ddubson.batch.domain.Customer
-import com.ddubson.batch.domain.CustomerRowMapper
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepScope
-import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.item.ItemWriter
-import org.springframework.batch.item.database.JdbcCursorItemReader
-import org.springframework.batch.item.database.JdbcPagingItemReader
-import org.springframework.batch.item.database.Order
-import org.springframework.batch.item.database.support.MySqlPagingQueryProvider
-import org.springframework.batch.repeat.RepeatStatus
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.batch.item.file.FlatFileItemReader
+import org.springframework.batch.item.file.mapping.DefaultLineMapper
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javax.sql.DataSource
+import org.springframework.core.io.ClassPathResource
 
 @Configuration
 class JobConfiguration(val jobBuilderFactory: JobBuilderFactory,
-                       val stepBuilderFactory: StepBuilderFactory,
-                       val dataSource: DataSource) {
-    /** Non-thread safe **/
-    //@Bean
-    fun cursorItemReader(): JdbcCursorItemReader<Customer> {
-        val reader = JdbcCursorItemReader<Customer>()
-        reader.sql = "select id, firstName, lastName, birthdate from customer order by lastName, firstName"
-        reader.dataSource = this.dataSource
-        reader.setRowMapper(CustomerRowMapper())
+                       val stepBuilderFactory: StepBuilderFactory) {
+    @Bean
+    fun customerItemReader(): FlatFileItemReader<Customer> {
+        val reader = FlatFileItemReader<Customer>()
+        reader.setLinesToSkip(1)
+        reader.setResource(ClassPathResource("/customer.csv"))
+
+        val customerLineMapper = DefaultLineMapper<Customer>()
+        val tokenizer = DelimitedLineTokenizer()
+        tokenizer.setNames(arrayOf("id", "firstName", "lastName", "birthdate"))
+
+        customerLineMapper.setLineTokenizer(tokenizer)
+        customerLineMapper.setFieldSetMapper(CustomerFieldSetMapper())
+        customerLineMapper.afterPropertiesSet()
+
+        reader.setLineMapper(customerLineMapper)
 
         return reader
-    }
-
-    /** Thread-safe **/
-    @Bean
-    fun pagingItemReader(): JdbcPagingItemReader<Customer> {
-        val reader = JdbcPagingItemReader<Customer>()
-        reader.setDataSource(this.dataSource)
-        reader.setFetchSize(10) // fetch size should be close to chunk size
-        reader.setRowMapper(CustomerRowMapper())
-
-        val queryProvider: MySqlPagingQueryProvider = MySqlPagingQueryProvider()
-        queryProvider.setSelectClause("id, firstName, lastName, birthdate")
-        queryProvider.setFromClause("from customer")
-        queryProvider.sortKeys = mapOf(Pair("id", Order.ASCENDING))
-
-        reader.setQueryProvider(queryProvider)
-
-        return reader;
     }
 
     @Bean
     fun customerItemWriter(): ItemWriter<Customer> {
         return ItemWriter<Customer> { items ->
-            items.forEach { i -> println(i.toString()) }
+            items.forEach { item -> println(item) }
         }
     }
 
@@ -63,15 +46,15 @@ class JobConfiguration(val jobBuilderFactory: JobBuilderFactory,
     fun step1(): Step {
         return stepBuilderFactory.get("step1")
                 .chunk<Customer, Customer>(10)
-                .reader(cursorItemReader())
+                .reader(customerItemReader())
                 .writer(customerItemWriter())
                 .build()
     }
 
     @Bean
     fun job(): Job {
-        return jobBuilderFactory.get("job1")
+        return jobBuilderFactory.get("job")
                 .start(step1())
-                .build();
+                .build()
     }
 }
